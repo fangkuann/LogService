d6e5a61e409ae3aa7508b1f0235bbc3f1df8e4fe
fangkuann <fangkuann@gmail.com>
2014-08-23 20:30:57 +0800

    update file

--- server_demo.py
+++ server_demo.py
@@ -1,29 +1,38 @@
 import web
-#from view import render
+# from view import render
 from web import form
-urls=(
-"/(.*)", "index"
+
+urls = (
+    "/(.*)", "index"
 )
 
 login = web.form.Form(
-	form.Textbox('username'),
-	form.Password('Password'),
-	form.Button('Login'),
-	)
+    form.Textbox('username'),
+    form.Password('Password'),
+    form.Button('Login'),
+)
+
 
 class index:
-	def GET(self, name):
-#		name = web.input(name=None)
-		render = web.template.render("templates/")
-#		name = 'fangkuan'
-		return render.welcome(name, login)
+    def GET(self, name):
+
+        render = web.template.render("templates/")
+        #		name = web.input(name=None)
+        #		name = 'fangkuan'
+        return render.welcome(name, login)
+
 
 class add:
-	def post(self):
-		i = web.input()	
-		#f=login()
-		#print f.render()
+    def post(self):
+        #i = web.input()
+        #f = login()
+        data = web.data()
+        #print data
+        render = web.template.render("templates/")
+        #return render.LoginSuccess()
+        raise web.seeother("/"+'templates/LginSuccess.html')
 
-if __name__=="__main__":
-	app = web.application(urls, globals())
-	app.run()
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
